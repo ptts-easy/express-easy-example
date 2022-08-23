9273fb5e098dae0f5f95af10fd21138fef1c9e9f
faco0909 <faco0909@outlook.com>
2022-08-23 03:20:54 +0100

    update

--- src/index.js
+++ src/index.js
@@ -6,20 +6,22 @@ app.get('/', function(req, res){
    res.send("Hello world!");
 });
 
-app.get('/users', (req, res) => {
-  return res.send('GET HTTP method on user resource');
+app.get('/restapi', (req, res) => {
+  return res.send('GET HTTP method for Read on restapi resource');
 });
 
-app.post('/users', (req, res) => {
-  return res.send('POST HTTP method on user resource');
+app.post('/restapi', (req, res) => {
+  return res.send('POST HTTP method for Create on restapi resource');
 });
 
-app.put('/users', (req, res) => {
-  return res.send('PUT HTTP method on user resource');
+app.put('/restapi', (req, res) => {
+  return res.send('PUT HTTP method for Update on restapi resource');
 });
 
-app.delete('/users', (req, res) => {
-  return res.send('DELETE HTTP method on user resource');
+app.delete('/restapi', (req, res) => {
+  return res.send('DELETE HTTP method for Delete on restapi resource');
 });
+
+console.log("Server Listend : ", process.env.SERVER_PORT);
 
 app.listen(process.env.SERVER_PORT);
